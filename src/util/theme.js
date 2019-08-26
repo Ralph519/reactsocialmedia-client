964b915d9561095cbeff924e743f5aa3d131994e
r.toledo <r.toledo@archriodon.net>
2019-08-26 11:46:24 +0400

    initial commit

---
+++ src/util/theme.js
@@ -0,0 +1,44 @@
+export default {
+    palette: {
+        primary: {
+        light: '#33c9dc',
+        main: '#00bcd4',
+        dark: '#008394',
+        contrastText: '#fff'
+        },
+        secondary: {
+        light: '#ff6333',
+        main: '#ff3d00',
+        dark: 'b22a00',
+        contrastText: '#fff'
+        }
+    },
+    typography: {
+        useNextVariants: true
+    },
+    spreadIt: {
+        form: {
+        textAlign: 'center'
+        },
+        image: {
+            margin: '20px auto 20 auto'
+        },
+        pageTitle: {
+            margin: '10px auto 10 auto'
+        },
+        textFields: {
+            margin: '10px auto 10 auto'
+        },
+        button: {
+            marginTop: 20
+        },
+        customError: {
+            color: 'red',
+            fontSize: '0.8rem',
+            marginTop: 10
+        },
+        progress: {
+            position: 'absolute'
+        }
+    }
+}
